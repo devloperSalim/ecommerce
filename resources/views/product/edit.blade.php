40dfc6fbe2feb206ee9083fc9d36d3d353ecf24f
salim_boulkhir <salimboulkhir123@gmail.com>
2024-04-20 21:47:03 +0100

    add stylento edit product

--- resources/views/product/edit.blade.php
+++ resources/views/product/edit.blade.php
@@ -1,34 +1,46 @@
 
 <x-master>
-    <form action="{{ route('products.update', $product->id) }}" method="POST" enctype="multipart/form-data">
+    
+    <form action="{{ route('products.update', $product->id) }}" method="POST" enctype="multipart/form-data" class="mt-4">
         @csrf
         @method('PUT')
-        <label for="">name</label>
-        <input type="text" name="name" value="{{ old('name', $product->name) }}"><br>
-        <label for="">description</label>
-        <textarea name="description" id="" cols="30" rows="10">{{ old('description', $product->description) }}</textarea><br>
-        <label for="">Quantity</label>
-        <input type="number" name="quantity" value="{{ old('quantity', $product->quantity) }}"><br>
-        <label for="">image</label>
-        <input type="file" name="image" id=""><br>
+        <div class="form-group">
+            <label for="name">Name</label>
+            <input type="text" name="name" value="{{ old('name', $product->name) }}" class="form-control" id="name">
+        </div>
+        <div class="form-group">
+            <label for="description">Description</label>
+            <textarea name="description" class="form-control" id="description" cols="30" rows="10">{{ old('description', $product->description) }}</textarea>
+        </div>
+        <div class="form-group">
+            <label for="quantity">Quantity</label>
+            <input type="number" name="quantity" value="{{ old('quantity', $product->quantity) }}" class="form-control" id="quantity">
+        </div>
+        <div class="form-group">
+            <label for="image">Image</label>
+            <input type="file" name="image" class="form-control-file" id="image">
+        </div>
         @if ($product->image)
-            <img src="{{ asset('storage/' . $product->image) }}" alt="Product Image"
-                style="max-width: 200px; height: auto;">
+            <img src="{{ asset('storage/' . $product->image) }}" alt="Product Image" class="img-fluid" style="max-width: 200px; height: auto;">
         @else
             <p>No image available</p>
         @endif
-        <br>
-        <label for="">price</label>
-        <input type="number" name="price" value="{{ old('price', $product->price) }}"><br>
-        <select name="category_id" id="">
-            <option value="" disabled>Select category</option>
-            @foreach ($categories as $category)
-                <option value="{{ $category->id }}" {{ $category->id == $product->category_id ? 'selected' : '' }}>
-                    {{ $category->name }}
-                </option>
-            @endforeach
-        </select>
-
-        <button type="submit">add</button>
+        <div class="form-group">
+            <label for="price">Price</label>
+            <input type="number" name="price" value="{{ old('price', $product->price) }}" class="form-control" id="price">
+        </div>
+        <div class="form-group">
+            <label for="category_id">Category</label>
+            <select name="category_id" class="form-control" id="category_id">
+                <option value="" disabled>Select category</option>
+                @foreach ($categories as $category)
+                    <option value="{{ $category->id }}" {{ $category->id == $product->category_id ? 'selected' : '' }}>
+                        {{ $category->name }}
+                    </option>
+                @endforeach
+            </select>
+        </div>
+        <button type="submit" class="btn btn-primary">Update</button>
     </form>
+
 </x-master>
