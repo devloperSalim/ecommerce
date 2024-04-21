1399545eb069d9d4f39470569f8423c8cae66314
salim_boulkhir <salimboulkhir123@gmail.com>
2024-04-21 23:37:50 +0100

    add style to products index

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -1,46 +1,58 @@
 <x-master>
-    <form action="{{ route('search') }}" method="GET">
-        @csrf
-        <label for="">search</label>
-        <input type="text"  name="query">
-        <button>search</button>
-    </form>
-    <table>
-        <tr>
-            <th>name</th>
-            <th>Category</th>
-            <th>description</th>
-            <th>image</th>
-            <th>quantity</th>
-            <th>price</th>
-            <th>Edit</th>
-        </tr>
-        @foreach ($products as $product )
-            <tr>
-            <td>{{ $product->name }}</td>
-            <td>
-                @if ($product->category)
-                <a href="{{ route('categories.show',$product->category_id) }}">
-                    {{ $product->category->name }}
-                </a>
-                @endif
-            </td>
-            <td>{!! $product->description !!}</td>
-            <td><img src="{{ asset('storage/' .$product->image) }}" alt=""></td>
-            <td>{{ $product->quantity }}</td>
-            <td>{{ $product->price }}</td>
-            <td><a href="{{ route('products.edit',$product->id) }}">edit</a></td>
-            <td>
-                <form action="{{ route('products.destroy',$product->id) }}" method="POST">
-                    @csrf
-                    @method('delete')
-                    <button>delete</button>
-                </form>
-            </td>
-            </tr>
-        @endforeach
-    </table>
-    <div>
-        {{ $products->links() }}
+    <div class="container">
+        <form class="mb-4" action="{{ route('search') }}" method="GET">
+            @csrf
+            <div class="form-group">
+                <label for="query">Search</label>
+                <input type="text" class="form-control" id="query" name="query" placeholder="Enter keyword">
+            </div>
+            <button type="submit" class="btn btn-primary">Search</button>
+        </form>
+
+        <div class="table-responsive">
+            <table class="table table-striped">
+                <thead class="thead-dark">
+                    <tr>
+                        <th>Name</th>
+                        <th>Category</th>
+                        <th>Description</th>
+                        <th>Image</th>
+                        <th>Quantity</th>
+                        <th>Price</th>
+                        <th>Edit</th>
+                        <th>Delete</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($products as $product)
+                        <tr>
+                            <td>{{ $product->name }}</td>
+                            <td>
+                                @if ($product->category)
+                                    <a href="{{ route('categories.show', $product->category_id) }}">{{ $product->category->name }}</a>
+                                @endif
+                            </td>
+                            <td>{!! $product->description !!}</td>
+                            <td><img src="{{ asset('storage/' . $product->image) }}" alt="" class="img-thumbnail" style="max-height: 100px;"></td>
+                            <td>{{ $product->quantity }}</td>
+                            <td>{{ $product->price }}</td>
+                            <td><a href="{{ route('products.edit', $product->id) }}" class="btn btn-primary">Edit</a></td>
+                            <td>
+                                <form action="{{ route('products.destroy', $product->id) }}" method="POST">
+                                    @csrf
+                                    @method('delete')
+                                    <button type="submit" class="btn btn-danger">Delete</button>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+
+        <div class="d-flex justify-content-center">
+            {{ $products->links() }}
+        </div>
     </div>
+
 </x-master>
