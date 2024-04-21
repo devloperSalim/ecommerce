8e3d417ed95ae378330e221285039e8e275f4650
salim_boulkhir <salimboulkhir123@gmail.com>
2024-04-21 13:20:32 +0100

    add style to create category

--- resources/views/category/create.blade.php
+++ resources/views/category/create.blade.php
@@ -1,8 +1,12 @@
 <x-master>
-    <form action="{{ route('categories.store') }}" method="POST">
-        @csrf
-        <label for="">nom cat</label>
-        <input type="text" name="name" id="">
-        <button>add</button>
-    </form>
+    <div class="container">
+        <form action="{{ route('categories.store') }}" method="POST">
+            @csrf
+            <div class="form-group">
+                <label for="">nom cat</label>
+                <input type="text" name="name" id="" class="form-control"/>
+            </div>
+            <button class="btn btn-primary">add</button>
+        </form>
+    </div>
 </x-master>
