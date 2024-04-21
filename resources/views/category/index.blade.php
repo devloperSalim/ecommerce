c77c92acb53446c27dd23ab03881f13854d3006f
salim_boulkhir <salimboulkhir123@gmail.com>
2024-04-21 23:36:02 +0100

    add stye to category index

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -1,27 +1,34 @@
 <x-master>
 
-<table class="table">
-    <thead>
-        <tr>
-            <th>Name</th>
-            <th>Action</th>
-        </tr>
-    </thead>
-    <tbody>
-       @foreach ($categories as $categorie )
-       <tr>
-            <td scope="row">{{ $categorie->name }}</td>
-            <td><a href="{{ route('categories.edit',$categorie->id) }}">Edit</a></td>
-            <td>
-                <form action="{{ route('categories.destroy',$categorie) }}" method="POST">
-                    @csrf
-                    @method('delete')
-                    <button>delete</button>
-                </form>
-            </td>
-            <td><a href="{{ route('categories.show',$categorie) }}">show</a></td>
-        </tr>
-       @endforeach
-    </tbody>
-</table>
+    <div class="table-responsive">
+        <table class="table table-striped">
+            <thead class="thead-dark">
+                <tr>
+                    <th>Name</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($categories as $categorie)
+                    <tr>
+                        <td>{{ $categorie->name }}</td>
+                        <td>
+                            <a href="{{ route('categories.edit', $categorie->id) }}" class="btn btn-primary">Edit</a>
+                        </td>
+                        <td>
+                            <form action="{{ route('categories.destroy', $categorie) }}" method="POST">
+                                @csrf
+                                @method('delete')
+                                <button type="submit" class="btn btn-danger">Delete</button>
+                            </form>
+                        </td>
+                        <td>
+                            <a href="{{ route('categories.show', $categorie) }}" class="btn btn-info">Show</a>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+
 </x-master>
